1211a6398d8aca8262579ada3be833eb1a3ca034
Oct1 <591102252@qq.com>
2019-03-04 17:46:09 +0800

    基本做完main

--- js/index.js
+++ js/index.js
@@ -1,25 +1,35 @@
 $(function(){
+  // 导航
+  let navBox = $(".nav");
+  let navNumber = navBox.find("li"); 
+  navNumber.hover(function() {
+    console.log("经过")
+    $(this).children('a').addClass('navActive');
+  }, function() {
+    $(this).children('a').removeClass('navActive');
+  });
+
 	// 轮播图 
-		var slideBox = $(".slideBox");
-    var ul = slideBox.find("ul");
-    var oneWidth = slideBox.find("ul li").eq(0).width();
-    var number = slideBox.find(".spanBox span");            //注意分号 和逗号的用法
-    var timer = null;
-    var sw = 0;                    
-    //每个span绑定click事件，完成切换颜色和动画，以及读取参数值
-    number.on("click",function (){
-      $(this).addClass("active").siblings("span").removeClass("active");
-      sw=$(this).index();
-      ul.animate({
-            "right":oneWidth*sw,    //ul标签的动画为向左移动；
-      });
+	let slideBox = $(".slideBox");
+  let ul = slideBox.find("ul");
+  let oneWidth = slideBox.find("ul li").eq(0).width();
+  let number = slideBox.find(".spanBox span");            //注意分号 和逗号的用法
+  let timer = null;
+  let sw = 0;                    
+  //每个span绑定click事件，完成切换颜色和动画，以及读取参数值
+  number.on("click",function (){
+    $(this).addClass("active").siblings("span").removeClass("active");
+    sw=$(this).index();
+    ul.animate({
+          "right":oneWidth*sw,    //ul标签的动画为向左移动；
     });
-    //左右按钮的控制效果
-    $(".next").stop(true,true).click(function (){
-       sw++;
-       if(sw==number.length){sw=0};
-       number.eq(sw).trigger("click");
-    });
+  });
+  //左右按钮的控制效果
+  $(".next").stop(true,true).click(function (){
+     sw++;
+     if(sw==number.length){sw=0};
+     number.eq(sw).trigger("click");
+  });
   $(".prev").stop(true,true).click(function (){
       sw--;
       if(sw==number.length){sw=0};
@@ -30,6 +40,28 @@ $(function(){
       sw++;
       if(sw==number.length){sw=0};
       number.eq(sw).trigger("click");
-      },2000);
+  },5000);
 
+  //产业特色
+  let special = $(".mainContent");
+  let specialList = special.children('div'); 
+  specialList.hover(function() {
+    console.log("经过")
+    $(this).children('.specialImg').find('img').animate({"width":'140%',"height":"140%"});
+  }, function() {
+    $(this).children('.specialImg').find('img').animate({"width":'110%',"height":"110%"});
+  });
+
+  //全景虚拟游---轮播图
+ 
+
+  //在线预订
+  let online = $(".online");
+  let onlineList = online.children('.OnlinePic'); 
+  onlineList.hover(function() {
+    console.log("经过")
+    $(this).children('div').addClass('onlineActive');
+  }, function() {
+    $(this).children('div').removeClass("onlineActive");
+  });
 })
